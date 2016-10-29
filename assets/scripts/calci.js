f29b61681c64ba0c6c1e947c5295c7bf30934c72
Dhrimeet <dhrimeet@gmail.com>
2016-10-29 14:24:22 +0530

    Promoting orphan scripts to functions

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,18 +1,26 @@
+function handleInput(input) {
+  $('#preview').html($('#preview').html() + input);
+}
+
+function handleDelete() {
+ $('#preview').html($('#preview').html().slice(0, -1));
+}
+
+function evaluateResult() {
+  $('#result').html(eval($('#preview').html()));
+}
+
 $(document).ready(function() {
   $('#calculator .input').click(function (){
     var input;
     if (this.dataset.keyType == "digit") {
-    input=this.dataset.digit;
+    handleInput(this.dataset.digit);
   } else if (this.dataset.keyType == "operator") {
-    input=this.dataset.operator;
+    handleInput (this.dataset.operator);
   }else if (this.dataset.keyType == "delete") {
-    $('#preview').html($('#preview').html().slice(0, -1));
+    handleDelete();
   }else if (this.dataset.keyType == "equals") {
-    $('#result').html(eval($('#preview').html()));
-
-  }
-  if (typeof(input) != 'undefined') {
-    $('#preview').html($('#preview').html() + input);
+    evaluateResult();
   }
 
 });
@@ -24,7 +32,7 @@ $('#calculator #delete').dblclick(function() {
 
  ['0','1','2','3','4','5','6','7','8','9']. forEach(function(digit){
     $(document).bind('keyup','digit',function() {
-      $('#preview').html($('#preview').html() + '0');
+     handleInput();
     });
    });
  });
