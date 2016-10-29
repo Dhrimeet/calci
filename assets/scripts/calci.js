bc57e56bfd1f99be9e32e00d2f2c956bc7991499
Dhrimeet <dhrimeet@gmail.com>
2016-10-29 14:29:45 +0530

    Done

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -32,7 +32,7 @@ $('#calculator #delete').dblclick(function() {
 
  ['0','1','2','3','4','5','6','7','8','9']. forEach(function(digit){
     $(document).bind('keyup','digit',function() {
-     handleInput();
+     handleInput(digit);
     });
    });
  });
