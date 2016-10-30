770f021459e476355766b55b053197a92921d7da
Dhrimeet <dhrimeet@gmail.com>
2016-10-30 14:12:35 +0530

    Done

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -2,51 +2,76 @@ var Calci = {
   init: function() {
     $('#calculator .input').click(function (){
       if (this.dataset.keyType == "digit") {
-      Calci.handleInput(this.dataset.digit);
-    } else if (this.dataset.keyType == "operator") {
-      Calci.handleInput (this.dataset.operator);
-    }else if (this.dataset.keyType == "delete") {
-      Calci.handleDelete();
-    }else if (this.dataset.keyType == "equals") {
-      Calci.evaluateResult();
-    }
-   });
-   $('#calculator #delete').dblclick(function() {
-     Calci.clearResult();
-   });
-   ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '+', '-',]. forEach(function(digit){
+        Calci.handleInput(this.dataset.digit);
+      } else if (this.dataset.keyType == "operator") {
+        Calci.handleOperator(this.dataset.operator);
+      }else if (this.dataset.keyType == "delete") {
+        Calci.handleDelete();
+      }else if (this.dataset.keyType == "equals") {
+        Calci.evaluateResult();
+      }
+    });
+    $('#calculator #delete').dblclick(function() {
+      Calci.clearPreview();
+      Calci.clearResult();
+    });
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]. forEach(function(digit){
       $(document).bind('keyup',digit,function() {
-       Calci.handleInput(digit);
+        Calci.handleInput(digit);
       });
-     });
-  $(document).bind('keyup', '.' , function() {
-    lastNumber = Calci.getLastNumber();
-    if (lastNumber.indexOf('.') == -1) {
-      Calci.handleInput('.');
-    }
-  });
-  $(document).bind('keyup', 'backspace' , function() {
-    Calci.handleDelete();
-  });
-  $(document).bind('keyup', 'shift+=' , function() {
-    Calci.handleInput('+');
-  });
-  ['=', 'return'].forEach(function(key) {
-  $(document).bind('keyup', key , function() {
-    Calci.evaluateResult('=');
-  });
-});
-},
-   handleInput: function (input) {
+    });
+    ['/', '*', '+', '-',]. forEach(function(digit){
+      $(document).bind('keyup',digit,function() {
+        Calci.handleOperator(digit);
+      });
+    });
+    $(document).bind('keyup', '.' , function() {
+      lastNumber = Calci.getLastNumber();
+      if (lastNumber.indexOf('.') == -1) {
+        if (lastNumber.length == 0) {
+          Calci.handleInput(0);
+        }
+        Calci.handleInput('.');
+      }
+    });
+    $(document).bind('keyup', 'backspace' , function() {
+      Calci.handleDelete();
+    });
+    $(document).bind('keyup', 'shift+=' , function() {
+      Calci.handleOperator('+');
+    });
+    ['=', 'return'].forEach(function(key) {
+      $(document).bind('keyup', key , function() {
+        Calci.evaluateResult('=');
+      });
+    });
+  },
+  handleInput: function (input) {
     $('#preview').html($('#preview').html() + input);
   },
-   handleDelete: function() {
-   $('#preview').html($('#preview').html().slice(0, -1));
-   if ($('#preview').html().length==0){
-     Calci.clearResult();
-   }
- },
-    evaluateResult: function() {
+  handleOperator: function(operator) {
+    console.log(operator)
+    if ($('#preview').html().length == 0) {
+      if (operator == '-') {
+        Calci.handleInput('-');
+      }
+    }else {
+      lastChar = Calci.getLastChar();
+      console.log("bbb")
+      if (['+', '-', '*', '/'].indexOf(lastChar) != -1) {
+        Calci.handleDelete();
+      }
+      Calci.handleInput(operator);
+    }
+
+  },
+  handleDelete: function() {
+    $('#preview').html($('#preview').html().slice(0, -1));
+    if ($('#preview').html().length==0){
+      Calci.clearResult();
+    }
+  },
+  evaluateResult: function() {
     $('#result').html(eval($('#preview').html()));
   },
   clearResult: function(){
@@ -62,9 +87,22 @@ var Calci = {
     } else {
       return matches[0].slice(1)
     }
+  },
+  clearPreview: function() {
+    $('#preview').html('');
+  },
+  getLastChar: function() {
+    str = $('#preview').html();
+    console.log("lastChar" + str);
+    if (str.length == 0) {
+      return str;
+    } else {
+      return str[str.length - 1];
+    }
   }
+
 
 };
 $(document).ready(function() {
-   Calci.init();
+  Calci.init();
 });
